2bb591fe9469c2b089a94f8cb246af7ede91a89f
DESKTOP-87DI9D5\joaqu <joaquinfragon@gmail.com>
2025-02-06 17:24:32 +0100

    FEAT: se prepara el entorno

--- backend/src/cars/dto/update-car.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateCarDto } from './create-car.dto';
-
-/**
- * @deprecated Use CreateCarDto
- */
-export class UpdateCarDto extends PartialType(CreateCarDto) {}
